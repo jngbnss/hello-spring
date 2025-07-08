c4c4e6e452dc2384e2e30510b6553f802ed2d8d3
jngbn <[user email]>
2025-07-08 10:20:51 +0900

    View 환경설정 확인

---
+++ study/hello-spring/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "어트리뷰트벨류가 뭘까!!");
+        return "hello";
+    }
+}
