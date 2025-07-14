b7015ab607c957ca5c87c9de90713cbc2b13d789
jngbnss <jngbnmn@naver.com>
2025-07-14 11:56:37 +0900

    회원 웹 기능 - 등록

--- study/hello-spring/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ study/hello-spring/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -4,7 +4,9 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SpringConfig {
     @Bean
     public MemberService memberService(){
--- study/hello-spring/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ study/hello-spring/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -17,9 +17,6 @@ public class MemberService {
             this.memberRepository = memberRepository;
         }
 
-    /**
-     * 회원가입
-     */
     public Long join(Member member){
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
@@ -33,15 +30,11 @@ public class MemberService {
                 });
     }
 
-    /**
-     * 전체 회원 조회
-     */
     public List<Member> findMembers(){
         return memberRepository.findAll();
     }
 
     public Optional<Member>findOne(Long memberId){
         return memberRepository.findById(memberId);
-        // 옵셔널이 뭐지?
     }
 }
