af965f0bb2da936fb0617fe5ea56d07c976c103b
jngbnss <jngbnmn@naver.com>
2025-07-14 11:33:49 +0900

    스프링 빈 설정 완료

---
+++ study/hello-spring/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,18 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+
+public class SpringConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
